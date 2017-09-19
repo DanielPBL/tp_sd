90e421ab29e48ead6dd694eb3c14cd590649deed
Daniel Lopes <danielllopes11@gmail.com>
2017-09-19 10:13:52 -0300

    Testando - incompleto

--- TP_1/tests/src/exec_test.cpp
+++ TP_1/tests/src/exec_test.cpp
@@ -4,5 +4,7 @@
 namespace {
   TEST(ExecTest, ReturnString) {
     EXPECT_EQ("Olá mundo", exec("echo -n \"Olá mundo\""));
+    EXPECT_EQ("daniel    2994  0.0  0.0  22960  5632 pts/2    Ss   07:09   0:00 bash\n",
+      exec("grep bash maquina.1.log"));
   }
 }  // namespace
---
+++ TP_1/tests/src/server_test.cpp
@@ -0,0 +1,7 @@
+#include "server.hpp"
+#include "gtest/gtest.h"
+
+namespace {
+  TEST(ServerTest, Constructors) {
+  }
+}  // namespace
